a42f5bf7a4db695a486c793cfe290bc56d40ca40
Aurore Dimech <aurore.dimech@edu.devinci.fr>
2025-10-17 16:07:35 +0200

    feat: first test

--- index.test.ts
+++ index.test.ts
@@ -1,7 +1,30 @@
 import { describe, test, expect } from "@jest/globals"
 
-describe('...', () =>{
-    test('1', () =>{
-        expect(4+4).toEqual(9)
+describe('Panier', () =>{
+    test('devrait afficher le panier', () =>{
+
+        //ARRANGE
+        const cart: Cart = new Cart(
+            [
+                new Article(
+                    "Table",
+                    50
+                ),
+                new Article(
+                    "Chaise",
+                    20
+                ),
+                new Article(
+                    "Tabouret",
+                    10
+                )
+            ]
+        )
+
+        //ACT
+        const display = displayCart(cart)
+        //ASSERT
+        expect(display.content.map(it => it.name)).toEqual(["Table", "Chaise", "Tabouret"])
+        expect(display.content.map(it => it.price)).toEqual([50, 20, 10])
     })
 })
